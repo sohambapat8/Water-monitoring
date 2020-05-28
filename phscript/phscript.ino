7e503aefd8b1b772c1b6a67312361f494eac0a57
Soham <sohambapat8@gmail.com>
2020-05-28 22:01:33 +0100

    Simple pH level monitoring script

---
+++ phscript/phscript.ino
@@ -0,0 +1,40 @@
+#define phsensor 0
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println("test");
+}
+
+void loop() {
+
+  float totalvalue = 0;
+
+  for(int i=0; i<20; i++){
+    totalvalue+=analogRead(phsensor);
+    delay(50);
+  }
+
+  float phvalue = 3.5*totalvalue*5.0/1024/20; //converting the total value to mmillivolt then pH.
+
+  Serial.print("pH level is ");
+  Serial.print(phvalue,2); //output to 2 d.p.
+
+
+  if (phvalue >= 8.5){
+    Serial.print(" . This is alkaline and not safe to drink.");
+  }
+
+  else if (phvalue <= 6.5){
+    Serial.print(" . This is acidic and not safe to drink.");
+  }
+
+  else{
+    Serial.print(" . This is safe to drink.");
+  }
+
+  Serial.println(" ");
+
+  delay(1600);
+  
+
+}
