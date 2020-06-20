8f53779e8f7b0b48c994dbb4880a6edb595b1c60
Soham <sohambapat8@gmail.com>
2020-06-20 16:52:08 +0100

    Combined ph and turbidity code, final working version

--- phturbidity_combined/phturbidity_combined.ino
+++ phturbidity_combined/phturbidity_combined.ino
@@ -1,5 +1,6 @@
-#define phsensor 0
-#define turbiditysensor 2
+#define phsensor 2
+#define turbiditysensor 3
+
 
 void setup() {
   Serial.begin(9600);
@@ -18,54 +19,34 @@ void loop() {
   Serial.print("pH level is ");
   Serial.print(phvalue,2); //output to 2 d.p.
 
-
-  if (phvalue >= 8.5){
-    Serial.print(". This is alkaline.");
-  }
-
-  else if (phvalue <= 6.5){
-    Serial.print(". This is acidic.");
-  }
-
-  else{
-    Serial.print(". This is safe to drink.");
-  }
-
   Serial.println(" ");
 
   float turbidity_totalvalue = 0;
-
-  turbidity_totalvalue = 0;
 
    for(int i=0; i<50; i++){
     turbidity_totalvalue+=analogRead(turbiditysensor);
   }
 
-//  Serial.println(analogRead(turbiditysensor));
+  float volt = ((turbidity_totalvalue/1024)*5)/50;
 
-  float volt = ((turbidity_totalvalue/1023)*5)/50;
-
-//  float volt = ((float)analogRead(turbiditysensor)/1023)*5;
-
-//  int test = analogRead(turbiditysensor);
-//
-//  volt += ((float)analogRead(sensorPin)/1023)*5;
-
-  Serial.println(volt);
+  Serial.print("Turbidity level is ");
+  Serial.print(volt);
+  Serial.println(" ");
+//  Serial.println(volt);
 
   turbidity_totalvalue = 0;
 
 
-  if (phValue <=8.5 && phValue >=6.5 && volt > 3.5){
-    println("Good water quality"); 
+  if (phvalue <=9.5 && phvalue >=6.5 && volt > 3.8){
+    Serial.println("Good water quality"); 
   }
 
-  else if (phValue <6.5 && 2<volt){
-    println("Warning, please check water");
+  else if (phvalue < 6.5 && volt > 3.5){
+    Serial.println("Warning, please check water");
   }
 
   else{
-    println("Poor water quality. Not advisable to drink");
+    Serial.println("Poor water quality. Not advisable to drink");
   }
 
   delay(2900);
